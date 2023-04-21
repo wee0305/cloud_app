ec4128473a1d6de8c279ce28a1c2692df57e898f
Grayson Chang <64624816+wee0305@users.noreply.github.com>
2023-04-21 19:56:11 +0800

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>About Us</title>
+    </head>
+    <body>
+        <?php
+        $txt1 = "Learn PHP";
+        $txt2 = "W3Schools.com";
+        $x = 5;
+        $y = 4;
+
+        echo "<h2>" . $txt1 . "</h2>";
+        echo "Study PHP at " . $txt2 . "<br>";
+        echo $x + $y;
+        ?>
+    </body>
+</html>
